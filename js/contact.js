93493a8b2964f6157dbd65f9ad7ce3274fdb6565
Jorge Estanislao Barsoba <jebarsoba@gmail.com>
2016-10-10 17:26:51 -0300

    Adding JS validations.

--- js/contact.js
+++ js/contact.js
@@ -1,16 +1,27 @@
-$(document).ready(function() {
-  $("#submit").click(function() {
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var message = $("#message").val();
+/*$(document).ready(function() {*/
+var vm = {
+    emailAddress: ko.observable().extend({ required: true }).extend({ email: true }),
+    submit: function() {
+      if (vm.errors().length === 0) {
+        var name = $("#name").val();
+        var email = $("#email").val();
+        var message = $("#message").val();
 
-    $.post("send-email.php", {
-        name1: name,
-        email1: email,
-        message1: message
-      }, function(data, status) {
-          $("#contact-form").css("display", "none");
-          $("#returnmessage").css("display", "block");
-      });
-  });
-});
+        $.post("send-email.php", {
+            name1: name,
+            email1: email,
+            message1: message
+          }, function(data, status) {
+              $("#contact-form").css("display", "none");
+              $("#returnmessage").css("display", "block");
+          });
+      }
+      else {
+          vm.errors.showAllMessages();
+      }
+    }
+}
+
+vm.errors = ko.validation.group(vm);
+
+ko.applyBindings(vm);
